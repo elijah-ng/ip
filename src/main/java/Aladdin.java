c13f0c30b77d6b9549b26740ea7b27a08c497030
Elijah Ng Ding Jie <106311806+elijah-ng@users.noreply.github.com>
2026-01-18 15:31:33 +0800

    Rename chatbot to Aladdin and implement initial greeting

---
+++ src/main/java/Aladdin.java
@@ -0,0 +1,10 @@
+public class Aladdin {
+    public static void main(String[] args) {
+        String chatbotName = "Aladdin";
+
+        System.out.println("Hello! I am " + chatbotName + "!");
+        System.out.println("What can I do for you?\n");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
