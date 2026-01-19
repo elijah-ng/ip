0ff2dfbee1e84adf62ee9ac412a7300d8790b7af
Elijah Ng Ding Jie <106311806+elijah-ng@users.noreply.github.com>
2026-01-19 13:30:09 +0800

    Implement error handling for Aladdin chatbot
    Let's,
    * Implement error handling for invalid user inputs
    * Update automated text UI testing to test for error handling

--- src/main/java/Aladdin.java
+++ src/main/java/Aladdin.java
@@ -39,12 +39,19 @@ public class Aladdin {
      *
      * @param taskString Task to be added to taskList.
      * @return true if the task is successfully added. Otherwise, false.
+     * @throws AladdinException if task format is invalid.
      */
-    private boolean addTask(String taskString) {
+    private boolean addTask(String taskString) throws AladdinException {
         // If task list is not full
         if (this.taskCount < 100) {
             // Split into max 2 substrings
             String[] addTaskCommand = taskString.split(" ", 2);
+
+            // If there is no task description, addTaskCommand has length == 1.
+            // or if the description is blank (empty or contain only whitespaces)
+            if ((addTaskCommand.length != 2) || (addTaskCommand[1].isBlank())) {
+                throw new AladdinException("Invalid task! The description of a task cannot be empty/blank.");
+            }
 
             if (addTaskCommand[0].equals("todo")) {
                 // Add todo task to taskList
@@ -53,32 +60,29 @@ public class Aladdin {
             } else if (addTaskCommand[0].equals("deadline")) {
                 String[] deadlineString = addTaskCommand[1].split(" /by ", 2);
                 if (deadlineString.length != 2) {
-                    System.out.println("Invalid deadline format. Please specify /by {date/time}.");
-                    return false;
+                    throw new AladdinException("Invalid deadline format. " +
+                            "Please specify {description} /by {date/time}.");
                 }
                 // Add deadline task to taskList
                 this.taskList[this.taskCount] = new Deadline(deadlineString[0], deadlineString[1]);
 
             } else if (addTaskCommand[0].equals("event")) {
+                String eventFormatError = "Invalid event format. " +
+                        "Please specify {description} /from {date/time} /to {date/time}.";
 
                 String[] eventString1 = addTaskCommand[1].split(" /from ", 2);
                 if (eventString1.length != 2) {
-                    System.out.println("Invalid event format. Please specify /from {date/time}.");
-                    return false;
+                    throw new AladdinException(eventFormatError);
                 }
 
                 String[] eventString2 = eventString1[1].split(" /to ", 2);
                 if (eventString2.length != 2) {
-                    System.out.println("Invalid event format. Please specify /to {date/time}.");
-                    return false;
+                    throw new AladdinException(eventFormatError);
                 }
 
                 // Add Event task to taskList
                 this.taskList[this.taskCount] = new Event(eventString1[0], eventString2[0], eventString2[1]);
 
-            } else {
-                System.out.println("Invalid type of task! Please enter todo/deadline/event task, followed by a space.");
-                return false;
             }
 
             // Print the new task added
@@ -103,26 +107,28 @@ public class Aladdin {
      * @param taskNumber Specified task to mark or unmark.
      */
     private void changeTaskStatus(String userCommand, int taskNumber) {
-        System.out.println(LINE_SEP);
+        System.out.println(LINE_SEP); // Beginning line separator
 
         // If taskNumber is valid
         if ((0 < taskNumber) && (taskNumber <= this.taskCount)) {
             // Mark task as done
             if (userCommand.equals("mark")) {
                 this.taskList[taskNumber - 1].setDone(true);
-                System.out.println("Great Job! I have marked the task as done:\n" + this.taskList[taskNumber - 1]);
+                System.out.println("Great Job! I have marked the task as done:\n"
+                        + this.taskList[taskNumber - 1]);
 
             } else if (userCommand.equals("unmark")) {
                 // Unmark task as not done
                 this.taskList[taskNumber - 1].setDone(false);
-                System.out.println("Ok, I have marked the task as not done yet:\n" + this.taskList[taskNumber - 1]);
+                System.out.println("Ok, I have marked the task as not done yet:\n"
+                        + this.taskList[taskNumber - 1]);
             }
 
         } else {
             System.out.println("Task " + taskNumber + " does not exist");
         }
 
-        System.out.println(LINE_SEP);
+        System.out.println(LINE_SEP); // Ending line separator
     }
 
     /**
@@ -163,34 +169,48 @@ public class Aladdin {
         // Get user input until user types command "bye"
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
-            // Print taskList
-            if (userInput.equals("list")) {
-                chatbot.printTaskList();
+            try {
+                String[] userCommand = userInput.split(" ", 2);
 
-            } else if (userInput.startsWith("mark")
-                    || userInput.startsWith("unmark")) {
+                // Print taskList
+                if (userCommand[0].equals("list")) {
+                    chatbot.printTaskList();
 
-                String[] userCommand = userInput.split(" ");
-                int taskNumber = Integer.parseInt(userCommand[1]);
-                // Call method to change task status
-                chatbot.changeTaskStatus(userCommand[0], taskNumber);
+                } else if (userCommand[0].equals("mark")
+                        || userCommand[0].equals("unmark")) {
+                    // TODO: catch not integer exception
+                    int taskNumber = Integer.parseInt(userCommand[1]);
+                    // Call method to change task status
+                    chatbot.changeTaskStatus(userCommand[0], taskNumber);
 
-            } else if (userInput.startsWith("todo")
-                    || userInput.startsWith("deadline")
-                    || userInput.startsWith("event")) {
-                // Add task to taskList
-                chatbot.addTask(userInput);
+                } else if (userCommand[0].equals("todo")
+                        || userCommand[0].equals("deadline")
+                        || userCommand[0].equals("event")) {
+                    // Add task to taskList
+                    chatbot.addTask(userInput);
 
-            } else {
-                System.out.println("Invalid command. Please try again.");
+                } else {
+                    throw new AladdinException("Invalid command. Please try again.");
+                }
+
+            } catch (AladdinException e) {
+                System.out.println(LINE_SEP);
+                System.out.println("AladdinException: " + e.getMessage());
+                System.out.println(LINE_SEP);
+
+            } catch (NumberFormatException e) {
+                System.out.println(LINE_SEP);
+                System.out.println("NumberFormatException: Please enter a valid number. " + e.getMessage());
+                System.out.println(LINE_SEP);
+
+            } finally {
+                // Return if there is no user input.
+                if (!sc.hasNextLine()) {
+                    return;
+                }
+                // Get next user input
+                userInput = sc.nextLine();
             }
-
-            // Return if there is no user input.
-            if (!sc.hasNextLine()) {
-                return;
-            }
-            // Get next user input
-            userInput = sc.nextLine();
         }
 
         // Print exit message
