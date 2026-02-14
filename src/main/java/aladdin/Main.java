1747ca1b0a17c59ecb93931629a266e1b36fbf47
Elijah Ng Ding Jie <106311806+elijah-ng@users.noreply.github.com>
2026-02-14 22:56:43 +0800

    Improve GUI visuals and optimise space

--- src/main/java/aladdin/Main.java
+++ src/main/java/aladdin/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setAladdin(aladdin); // inject the Aladdin instance
+            stage.setTitle("Aladdin Chatbot");
 
             stage.show();
         } catch (IOException e) {
